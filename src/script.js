63342b2fa681caa833331b85f6fa8f05b6e47e88
sjoshi05 <121900096+sjoshi05@users.noreply.github.com>
2023-03-18 14:32:00 +0000

    Added Current Location to Drop Down

--- src/script.js
+++ src/script.js
@@ -26,26 +26,29 @@ function updateTime() {
   }
 }
 
-updateTime();
-setInterval(updateTime, 1000);
-
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
+  let cityTimeFormatted = cityTime.format("h[:]mm[:]ss [<small>]A[</small>]");
+  let cityDate = cityTime.format("MMMM D[,] YYYY");
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-    <div class="city">
-            <div>
-              <h2>${cityName}</h2>
-              <div class="date">${cityTime.format("MMMM D[,] YYYY")}</div>
-            </div>
-            <div class="time">${cityTime.format(
-              "h[:]mm[:]ss [<small>]A[</small>]"
-            )}</div>
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityDate}</div>
           </div>
-  `;
+          <div class="time">${cityTimeFormatted}</div>
+        </div>
+`;
 }
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
+updateTime();
+setInterval(updateTime, 1000);
